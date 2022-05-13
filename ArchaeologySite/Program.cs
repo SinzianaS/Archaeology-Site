c40cd6e3439c563a0c6323d8f2b9cd27e1bb5346
Sinziana Sovejanu <sv.sinziana@gmail.com>
2022-05-13 22:53:46 +0300

    adding PavementBlocks console app

---
+++ ArchaeologySite/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArchaeologySite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double verticeAx = Convert.ToDouble(Console.ReadLine());
+            double verticeAy = Convert.ToDouble(Console.ReadLine());
+            double verticeBx = Convert.ToDouble(Console.ReadLine());
+            double verticeBy = Convert.ToDouble(Console.ReadLine());
+            double verticeCx = Convert.ToDouble(Console.ReadLine());
+            double verticeCy = Convert.ToDouble(Console.ReadLine());
+            double minimalArea = 0.5 * (verticeAx * (verticeBy - verticeCy) + verticeBx * (verticeCy - verticeAy) + verticeCx * (verticeAy - verticeBy));
+            double v = Math.Abs(minimalArea);
+            Console.WriteLine(v);
+            Console.Read();
+        }
+    }
+}
+
